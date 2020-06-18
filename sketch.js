5e577e201f6171e1d240534ca38b54dae10a36b3
Marcelo Acosta <acosta.cavalero@gmail.com>
2020-06-17 21:01:47 -0400

    one dimensional perlin noise

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var inc = 0.05;
+var start = 0;
+
+function setup() {
+    createCanvas(400,400)
+}
+
+function draw() {
+    
+    
+    
+    background(51);
+    stroke(255);
+    noFill();
+    beginShape();
+    var xoff = start;
+    
+for (var x = 0; x < width; x++){
+    stroke(255);
+    //var y = random(height);
+    
+    var y = noise(xoff) * height;
+   
+    vertex(x,y);
+    
+   
+    xoff += inc;
+    
+}
+    
+
+    endShape();
+    
+     start += inc;
+    fill(255);
+     ellipse(x-5,y,10,10);
+    
+    
+}
+    
+
+    
+/*    var x = map(noise(xoff1),0,1,0,width);
+    var y = map(noise(xoff2),0,1,0,height);
+    xoff1 += 0.01;
+    xoff2 += 0.01;
+    
+    ellipse(x,y,24,24);
+    */
+    
+    
+    
+    
+
+
